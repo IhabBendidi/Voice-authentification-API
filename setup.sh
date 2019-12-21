00e0f9f8cf86fec4d72a706ff0fbb0237542cf8a
ihab bendidi <ihabnobendidi@gmail.com>
2019-12-21 11:54:44 +0100

    clean setup and requirements

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-sudo apt-get install -y libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+sudo apt-get install -y libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 
 sudo apt-get install -y ffmpeg libav-tools
 sudo pip install -y pyaudio
 python3 -m pip install -r requirements.txt
